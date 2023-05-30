81ac27d5e7aa24dd401dc8d6a8e725f8b7b52df2
Minato <fate1212@mail.ru>
2023-05-30 22:44:50 +0500

    css v1.0

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Profile.css'
+import ava from '../../img/ava.jpg'
+import ava_min from '../../img/ava_mini.jpg'
+function Profile() {
+    return(
+      <div className='profile'>
+        <div className='me'>
+          <img src={ava} alt='profile mini photo' />
+          <p>Elon mask</p>
+        </div>
+        <div className='posts'>
+          <input placeholder='enter the post' />
+          <button>Add post</button>
+          <div className='post'>
+          <img src={ava_min}  alt='profile mini photo' />
+          <span>Elon Mask</span>
+          <p>Some text</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  export default Profile
